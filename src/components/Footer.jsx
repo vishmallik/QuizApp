9e6184c07849a989facb78f88979228b9723e95a
visawjeet mallik <vishmallik@gmail.com>
2022-11-25 00:00:17 +0530

    add favicon , license and readme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className=" mx-auto bg-black text-center mt-4 py-6 text-white">
-      <p className="text-xl ">&copy; Visawjeet Mallik, 2022</p>
+      <p className="text-xl">&copy; Visawjeet Mallik, 2022</p>
       <a
-        href="https://github.com/vishmallik"
+        href="https://github.com/vishmallik/QuizApp"
         className="text-4xl py-2 inline-block "
       >
         <i className="fab fa-github hover:text-yellow-400"></i>
